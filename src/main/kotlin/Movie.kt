52f5f71ce4fe9ba32df3f9185c199cdfa8bf571b
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-05-26 14:00:06 +0530

    Infixing the methods

--- src/main/kotlin/Movie.kt
+++ src/main/kotlin/Movie.kt
@@ -4,9 +4,9 @@ data class Movie(val imdbID: String, val title:String, val releaseDate: Int,val 
         return "Movie(imdbID='$imdbID', title='$title', releaseDate=$releaseDate, geners=$geners, director='$director', actors=$actors, actress=$actress, duration=$duration)"
     }
 
-    fun hasReleaseYear(year : Int): Boolean = releaseDate == year
-    fun hasDurationGreaterThan(minDuration:  Int): Boolean = duration >= minDuration
-    fun hasActor(actor: String): Boolean = actors.contains(actor)
+    infix fun hasReleaseYear(year : Int): Boolean = releaseDate == year
+    infix fun hasDurationGreaterThan(minDuration:  Int): Boolean = duration >= minDuration
+    infix fun hasActor(actor: String): Boolean = actors.contains(actor)
 
 
 }
--- src/main/kotlin/MovieStore.kt
+++ src/main/kotlin/MovieStore.kt
@@ -4,10 +4,10 @@ class MovieStore(val movieList: List<Movie>) {
         if(movieList.size <5)  throw IllegalArgumentException()
     }
 
-    fun findMoviesByReleaseDate(year: Int):List<Movie> = movieList.filter { x -> x.hasReleaseYear(year) }
+    fun findMoviesByReleaseDate(year: Int):List<Movie> = movieList.filter { movie -> movie hasReleaseYear year }
 
-    fun findMoviesWithDurationGreaterThan(duration:Int):List<Movie> = movieList.filter { x -> x.hasDurationGreaterThan(duration) }
+    fun findMoviesWithDurationGreaterThan(duration:Int):List<Movie> = movieList.filter { movie -> movie hasDurationGreaterThan duration }
 
-    fun findMoviesWithActors(actor:String):List<Movie> = movieList.filter { x -> x.hasActor(actor) }
+    fun findMoviesWithActors(actor:String):List<Movie> = movieList.filter { movie -> movie hasActor actor }
 
 }
